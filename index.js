4b5253225d82b79b9f0e8fdac77f38a85669b528
navayaozi <navayaozi@hotmail.com>
2025-04-21 14:47:23 +0800

    initial project setup
    added basic package.json and entry point

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('NFT Floor Bot starting...');
+
+// TODO: Add Discord bot setup
+// TODO: Add OpenSea API integration
+// TODO: Add floor price tracking logic
+
+process.on('SIGINT', () => {
+  console.log('Bot shutting down...');
+  process.exit(0);
+});
