c5f522fc71126f6d8f33e6dff6fd640bcba431eb
navayaozi <navayaozi@hotmail.com>
2025-04-24 19:32:14 +0800

    add discord bot foundation
    - discord.js setup with basic client
    - ping command for testing
    - added dependencies to package.json

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-console.log('NFT Floor Bot starting...');
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
 
-// TODO: Add Discord bot setup
-// TODO: Add OpenSea API integration
-// TODO: Add floor price tracking logic
+const client = new Client({ 
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] 
+});
+
+client.once('ready', () => {
+  console.log(`Bot logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.author.bot) return;
+  
+  if (message.content === '!ping') {
+    message.reply('Pong!');
+  }
+  
+  // TODO: Add NFT floor price commands
+});
+
+client.login(process.env.DISCORD_TOKEN).catch(console.error);
 
 process.on('SIGINT', () => {
   console.log('Bot shutting down...');
+  client.destroy();
   process.exit(0);
 });
